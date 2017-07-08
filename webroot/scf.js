c8d7f46e4317a49f506f7052a357b2d80dfb34da
Tomas Libal <tomas@libal.eu>
2017-07-09 00:16:54 +0800

    fix dialog

--- webroot/scf.js
+++ webroot/scf.js
@@ -228,14 +228,14 @@ function addActionHandlers() {
             if (! dialog.showModal) {
                 dialogPolyfill.registerDialog(dialog);
             }
-            showDialogButton.addEventListener('click', function() {
-                dialog.showModal();
-            });
-            dialog.querySelector('.close').addEventListener('click', function() {
-                dialog.close();
-            });
+
+            dialog.showModal();
         }
     }, { passive: true, capture: true });
+
+    document.querySelector('dialog').querySelector('.close').addEventListener('click', function() {
+        dialog.close();
+    });
 }
 
 function init() {
